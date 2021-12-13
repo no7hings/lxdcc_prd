3b39f4413de910b545767f6641391258fa1dc66d
dongchangbao <dongchangbao@papegames.net>
2021-12-13 10:59:59 +0800

    lxgui: modify and add resource

--- parse_build_args.py
+++
@@ -1,3 +0,0 @@
-# Adds custom build arguments
-parser.add_argument('--symlink', action='store_true', help='Make symbolic link')
-parser.add_argument('--current', action='store_true', help='Build "current" folder')
